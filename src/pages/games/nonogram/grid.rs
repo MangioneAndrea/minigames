e0ef4b3e091b4b4795837c0d896949147959b808
Andrea Mangione <35958935+MangioneAndrea@users.noreply.github.com>
2022-11-17 22:29:33 +0100

    add column rules

--- src/pages/games/nonogram/grid.rs
+++ src/pages/games/nonogram/grid.rs
@@ -1,3 +1,5 @@
+use std::cmp;
+
 use yew::prelude::*;
 
 pub enum Message {
@@ -10,57 +12,88 @@ pub struct Grid {
     pub cols: usize,
     pub grid: Vec<Vec<bool>>,
 }
+#[derive(Properties, Clone, PartialEq)]
+pub struct GridProps {
+    pub rows: usize,
+    pub cols: usize,
+    pub rows_rules: Vec<Vec<usize>>,
+    pub cols_rules: Vec<Vec<usize>>,
+}
 
-impl Grid {
-    pub fn new(rows: usize, cols: usize) -> Self {
+#[function_component(Grid2)]
+pub fn grid(props: &GridProps) -> Html {
+    let gridb = use_state(|| {
         let mut grid = Vec::new();
-        for _ in 0..rows {
+        for _ in 0..props.rows {
             let mut row = Vec::new();
-            for _ in 0..cols {
+            for _ in 0..props.cols {
                 row.push(false);
             }
             grid.push(row);
         }
-        Self { rows, cols, grid }
-    }
+        grid
+    });
 
-    pub fn toggle(&mut self, row: usize, col: usize) {
-        self.grid[row][col] = !self.grid[row][col];
-    }
-}
+    let oncellclick = |grid: UseStateHandle<Vec<Vec<bool>>>, row: usize, col: usize| {
+        Callback::from(move |_: MouseEvent| {
+            grid.set({
+                let mut grid = (*grid).clone();
+                grid[row][col] = !grid[row][col];
+                grid.to_vec()
+            });
+        })
+    };
 
-impl Component for Grid {
-    type Message = Message;
-    type Properties = ();
+    let max_rows = {
+        let mut max = 0;
+        for row in &props.rows_rules {
+            max = cmp::max(max, row.len());
+        }
+        max
+    };
 
-    fn create(_: &Context<Self>) -> Self {
-        Self::new(10, 10)
-    }
+    let max_cols = {
+        let mut max = 0;
+        for col in &props.cols_rules {
+            max = cmp::max(max, col.len());
+        }
+        max
+    };
 
-    fn update(&mut self, _: &Context<Self>, msg: Self::Message) -> bool {
-        log::info!("hey");
-        match msg {
-            Message::Clicked(row, col) => {
-                self.toggle(row, col);
-                true
-            },
-            _ => {
-                false
+    log::info!("max_rows: {}", max_rows);
+    log::info!("max_cols: {}", max_cols);
+
+    html! {
+        <div class={format!("border-4 grid grid-cols-{} w-fit", props.cols+max_rows)}>
+            {
+                (0..(max_rows+props.cols)*max_cols)
+                    .map(|pos| html!{<div class="w-6 h-6 bg-green">{
+                        if pos%(max_rows+props.cols)*max_cols <max_rows{
+                            // empty angle
+                            format!("")
+                        }else{
+                            let col_elem= pos/((max_rows+props.cols));
+                            let col_index=pos%((max_rows+props.cols))-1;
+
+                            if props.cols_rules[col_index].len() > col_elem{
+                                format!("{}", props.cols_rules[col_index][col_elem])
+                            }else{
+                                format!("")
+                            }
+                        } }</div>})
+                    .collect::<Html>()
             }
-        }
-    }
+            { (0 .. (props).rows).map(|row|{
 
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        html! {
-            <div class={format!("border-4 grid grid-cols-{} w-fit", self.cols)}>
-                { (0 .. (self).cols).map(|col|{
-                    (0 .. self.rows).map(|row|{
-                        html! {
-                            <div class={format!("border margin w-6 h-6 bg-{}", if self.grid[col][row] {"black"}else{"white"})} onclick={ctx.link().callback(move |_| Message::Clicked(col,row))}/>
-                        }
-                    }).collect::<Html>()
-                }).collect::<Html>() }
-            </div>
-        }
+                (0..(max_rows))
+                    .map(|_| html!{<div class="w-6 h-6 bg-green">{"0"}</div>})
+                    .chain(
+                        (0 .. props.cols).map(|col|{
+                            html! {
+                                <div class={format!("border margin w-6 h-6 bg-{}", if gridb[row][col] {"black"}else{"white"})} onclick={oncellclick(gridb.clone(),row.clone(), col.clone())}/>
+                            }
+                })).collect::<Html>()
+            }).collect::<Html>() }
+        </div>
     }
 }
--- src/pages/games/nonogram/nonogram.rs
+++ src/pages/games/nonogram/nonogram.rs
@@ -1,5 +1,5 @@
+use crate::pages::games::nonogram::grid::Grid2;
 use yew::prelude::*;
-use crate::pages::games::nonogram::grid::Grid;
 
 #[function_component(Nonogram)]
 pub fn nonogram() -> Html {
@@ -7,7 +7,17 @@ pub fn nonogram() -> Html {
         <div>
             <h1>{"Nonogram"}</h1>
             <p>{"Nonogram is a game where you have to fill in a grid with black or whites. Rows and columns tell you what how many blocks should be conseguetially painted black"}</p>
-            <Grid/>
+            <Grid2 cols={4} rows={4} rows_rules={vec![
+                vec![],
+                vec![3],
+                vec![],
+                vec![1],
+            ]} cols_rules={vec![
+                vec![],
+                vec![],
+                vec![],
+                vec![1,1],
+            ]}/>
         </div>
     }
 }
